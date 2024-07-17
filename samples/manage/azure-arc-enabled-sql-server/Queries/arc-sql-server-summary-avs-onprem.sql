698f6b3e886481bcbc1e7e2f54c74341de312628
Derek Nguyen <62448881+dereknguyenio@users.noreply.github.com>
2024-07-17 15:52:03 -0500

    Create arc-sql-server-summary-avs-onprem.sql

---
+++ samples/manage/azure-arc-enabled-sql-server/Queries/arc-sql-server-summary-avs-onprem.sql
@@ -0,0 +1,55 @@
+UPDATED KUSTO QUERY - Arc SQL Server Summary - Includes AVS + Onpremise
+ 
+Resources
+| where type == 'microsoft.hybridcompute/machines'
+| extend SubscriptionId = tostring(split(id, "/")[2])
+| extend ResourceGroupName = tostring(split(id, "/")[4])
+| extend MachineId = toupper(id)
+| extend Kind = tostring(kind)
+| project
+    id,
+    SubscriptionId,
+    ResourceGroupName,
+    MachineId,
+    Kind,
+    ComputerName = tostring(properties.osProfile.computerName),
+    OSName = tostring(properties.osName),
+    ArcMachineStatus = tostring(properties.status),
+    SqlDetected = tobool(coalesce(properties.detectedProperties.mssqldiscovered, properties.mssqlDiscovered)),
+    Model = tostring(coalesce(properties.detectedProperties.model, properties.model))
+| where SqlDetected == true and (Kind == '' or Kind == 'VMWare' or Kind == 'AVS')
+| join kind=leftouter (
+    Resources
+    | where type == "microsoft.azurearcdata/sqlserverinstances"
+    | where properties.containerResourceId != ""
+    | project 
+        ArcMachineId = toupper(tostring(properties.containerResourceId)),
+        ServerInstanceName = tostring(properties.instanceName),
+        vCores = toint(properties.vCore),
+        SqlVersion = tostring(properties.version),
+        SqlEdition = tostring(properties.edition),
+        SqlServerStatus = tostring(properties.status),
+        CurrentSQLServerVersion = tostring(properties.currentVersion),
+        ProductId = tostring(properties.productId),
+        PatchLevel = tostring(properties.patchLevel),
+        AzureDefenderStatus = tostring(properties.azureDefenderStatus)
+) on $left.MachineId == $right.ArcMachineId
+| join kind=leftouter (
+    Resources
+    | where type == 'microsoft.hybridcompute/machines/extensions'
+    | project
+        ExtensionMachineId = toupper(substring(id, 0, indexof(id, '/extensions'))),
+        ExtensionName = name,
+        ArcSqlLicenseType = tostring(properties.settings.LicenseType),
+        SqlServerExtensionVersion = iff(properties.type == "WindowsAgent.SqlServer", properties.instanceView.typeHandlerVersion, ""),
+        SqlServerExtensionStatus = iff(properties.type == "WindowsAgent.SqlServer", properties.provisioningState, "")
+        //SqlServerExtensionMessage = iff(properties.type == "WindowsAgent.SqlServer", properties.instanceView.status.message, "")
+) on $left.MachineId == $right.ExtensionMachineId
+| summarize ArcSqlLicenseType = any(ArcSqlLicenseType), Extensions = make_list(ExtensionName), 
+    SqlServerExtensionVersion = any(SqlServerExtensionVersion), SqlServerExtensionStatus = any(SqlServerExtensionStatus)
+    //SqlServerExtensionMessage = any(SqlServerExtensionMessage)  
+    by id, SubscriptionId, ResourceGroupName, Kind, ComputerName, Model, OSName, ArcMachineStatus,  
+    ServerInstanceName, vCores, SqlVersion, SqlEdition, SqlServerStatus, 
+    CurrentSQLServerVersion, ProductId, PatchLevel, AzureDefenderStatus
+| extend SqlServerExtensionExists = iff(array_index_of(Extensions, "WindowsAgent.SqlServer") != -1, "Yes", "No")
+| order by tolower(OSName) asc
